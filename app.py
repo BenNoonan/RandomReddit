6ac06f1298bc6a76f661860c298a58d6cc1c2803
BenNoonan <btn9@txstate.edu>
2017-02-06 00:16:10 -0600

    Added html page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+app.config['TEMPLATE_AUTO_RELOAD'] = True
 
-@app.route('/')
+
+@app.route("/")
 def random_reddit():
-	return 'test'
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
