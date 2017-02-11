b97112da76be9d51863c8fe47fc3d4452b957616
BenNoonan <btn9@txstate.edu>
2017-02-11 17:31:52 -0600

    r/Funny json data page

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import requests
+import oauth2
+import urllib, json
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
@@ -6,10 +9,14 @@ app.config['TEMPLATE_AUTO_RELOAD'] = True
 
 
 @app.route("/")
-def random_reddit():
+def home():
 	return render_template('index.html')
 
-#@app.route("/random")
+@app.route("/json")
+def search():
+	r = requests.get('http://reddit.com/r/funny/.json?count=25&after=t3_10omtd/', headers = {'User-agent': 'Chrome'})
+	data = r.json()
+	return str(data)
 
 if __name__ == "__main__":
     app.run()
