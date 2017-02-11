e67e465d1434216f703839bedfcafa1e761add8e
BenNoonan <btn9@txstate.edu>
2017-02-11 17:34:42 -0600

    Added html page (#3)
    * Added html page
    
    * Modified html page per request
    
    * Large changes to web page
    
    * Modified front page of site
    
    * r/Funny json data page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import oauth2
+import urllib, json
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+app.config['TEMPLATE_AUTO_RELOAD'] = True
 
-@app.route('/')
-def random_reddit():
-	return 'test'
+
+@app.route("/")
+def home():
+	return render_template('index.html')
+
+@app.route("/json")
+def search():
+	r = requests.get('http://reddit.com/r/funny/.json?count=25&after=t3_10omtd/', headers = {'User-agent': 'Chrome'})
+	data = r.json()
+	return str(data)
 
 if __name__ == "__main__":
     app.run()
