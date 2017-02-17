861f796ccdec63cd4e6dd9b1c6ad3c259cd5b3e1
BenNoonan <btn9@txstate.edu>
2017-02-16 21:03:42 -0600

    random post on r/Funny (#6)
    * Added html page
    
    * Modified html page per request
    
    * Large changes to web page
    
    * Modified front page of site
    
    * r/Funny json data page
    
    * random post from r/funny added

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from flask import Flask, render_template
 import requests
 import oauth2
 import urllib, json
+from random import randint
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['TEMPLATE_AUTO_RELOAD'] = True
 
-
 @app.route("/")
 def home():
 	return render_template('index.html')
 
-@app.route("/json")
+@app.route("/funny")
 def search():
-	r = requests.get('http://reddit.com/r/funny/.json?count=25&after=t3_10omtd/', headers = {'User-agent': 'Chrome'})
-	data = r.json()
-	return str(data)
+	r = requests.get('http://reddit.com/r/funny/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
+	reddit_data = json.loads(r.text)
+	post_rand = randint(0, 24)
+	post_title = reddit_data["data"]["children"][post_rand]["data"]["title"]
+	post_source =  reddit_data["data"]["children"][post_rand]["data"]["preview"]["images"][0]["source"]["url"]
+
+  return (post_title + "\n" + post_source)
 
 if __name__ == "__main__":
     app.run()
