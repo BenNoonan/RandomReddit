963324970fff910e620b9005d8d0a2f983d1d0b9
BenNoonan <btn9@txstate.edu>
2017-02-16 23:22:48 -0600

    Fixed issue #9 (#10)
    * Added html page
    
    * Modified html page per request
    
    * Large changes to web page
    
    * Modified front page of site
    
    * r/Funny json data page
    
    * random post from r/funny added
    
    * Post display for anything subreddit

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ app.config['TEMPLATE_AUTO_RELOAD'] = True
 def home():
 	return render_template('index.html')
 
-@app.route("/funny")
-def search():
-	r = requests.get('http://reddit.com/r/funny/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
+@app.route("/<subreddit>")
+def post_finder(subreddit):
+	r = requests.get('http://reddit.com/r/' + subreddit + '/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
 	reddit_data = json.loads(r.text)
 	post_rand = randint(0, 24)
 	post_title = reddit_data["data"]["children"][post_rand]["data"]["title"]
 	post_source =  reddit_data["data"]["children"][post_rand]["data"]["preview"]["images"][0]["source"]["url"]
+	post_desc = reddit_data["data"]["children"][post_rand]["data"]["selftext"]
 
-  return (post_title + "\n" + post_source)
+	return render_template('funny.html', data_title = post_title, data_source = post_source) #(post_title + "\n" + post_source)
 
 if __name__ == "__main__":
     app.run()
