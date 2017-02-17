1ffaa118b7b3f5c761148607c9f580a537f4e679
BenNoonan <btn9@txstate.edu>
2017-02-17 00:07:14 -0600

    Fixed image display error

--- app.py
+++ app.py
@@ -17,11 +17,21 @@ def post_finder(subreddit):
 	r = requests.get('http://reddit.com/r/' + subreddit + '/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
 	reddit_data = json.loads(r.text)
 	post_rand = randint(0, 24)
-	post_title = reddit_data["data"]["children"][post_rand]["data"]["title"]
-	post_source =  reddit_data["data"]["children"][post_rand]["data"]["preview"]["images"][0]["source"]["url"]
-	post_desc = reddit_data["data"]["children"][post_rand]["data"]["selftext"]
 
-	return render_template('funny.html', data_title = post_title, data_source = post_source) #(post_title + "\n" + post_source)
+	post_title = reddit_data["data"]["children"][post_rand]["data"]["title"]
+	
+	if 'preview' in reddit_data["data"]["children"]:
+		post_source =  reddit_data["data"]["children"][post_rand]["data"]["preview"]["images"][0]["source"]["url"]
+	else:
+		post_source = None
+
+	if 'selftext' in reddit_data["data"]["children"]:
+		post_desc = reddit_data["data"]["children"][post_rand]["data"]["selftext"]
+	else:
+		post_desc = None
+
+	return render_template('funny.html', data_title = post_title, data_source = post_source, data_desc = post_desc
+	, data_sub = subreddit) 
 
 if __name__ == "__main__":
     app.run()
