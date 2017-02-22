0343293293fa0019cc824b9e09b19910562762a3
BenNoonan <btn9@txstate.edu>
2017-02-21 22:30:31 -0600

    Tweaked based on chu's request

--- app.py
+++ app.py
@@ -16,21 +16,18 @@ def home():
 def post_finder(subreddit):
 	r = requests.get('http://reddit.com/r/' + subreddit + '/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
 	reddit_data = json.loads(r.text)
-
 	max_post = len(reddit_data["data"]["children"])
 	post_rand = randint(0, max_post)
 
 	post_title = reddit_data["data"]["children"][post_rand]["data"]["title"]
+	post_source = "static/img/unavailable_img.png"
+	post_desc = "No Description"
 	
 	if 'preview' in reddit_data["data"]["children"][post_rand]["data"]:
 	 	post_source = reddit_data["data"]["children"][post_rand]["data"]["preview"]["images"][0]["source"]["url"]
-	else:
-	 	post_source = "static/img/unavailable_img.png"
 
 	if 'selftext' in reddit_data["data"]["children"][post_rand]["data"]:
 	 	post_desc = reddit_data["data"]["children"][post_rand]["data"]["selftext"]
-	else:
-		post_desc = "No Description"
 
 	return render_template('random.html', data_title = post_title, data_source = post_source, data_desc = post_desc
 	, data_sub = subreddit) 
