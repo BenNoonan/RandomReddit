1ca140e7f7d880f325658f24c09dba72da5c4056
BenNoonan <btn9@txstate.edu>
2017-03-06 19:38:02 -0600

    Added 404 page RESOLVED #11 (#15)
    * 404 page added
    
    * fix 404

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, redirect, url_for
 import requests
+from requests.exceptions import HTTPError
 import oauth2
 import urllib, json
 from random import randint
@@ -14,8 +15,14 @@ def home():
 
 @app.route("/subreddit/<subreddit>")
 def post_finder(subreddit):
-	r = requests.get('http://reddit.com/r/' + subreddit + '/top/.json', headers = {'User-agent': 'Chrome'})#.read().decode("utf-8")
+	r = requests.get('http://reddit.com/r/' + subreddit + '/top/.json', headers = {'User-agent': 'Chrome'})
 	reddit_data = json.loads(r.text)
+
+	if 'error' in reddit_data:
+		abort(404)
+	elif reddit_data["data"]["after"] == None:
+		abort(404)
+
 	max_post = len(reddit_data["data"]["children"])
 	post_rand = randint(0, max_post)
 
@@ -30,7 +37,12 @@ def post_finder(subreddit):
 	 	post_desc = reddit_data["data"]["children"][post_rand]["data"]["selftext"]
 
 	return render_template('random.html', data_title = post_title, data_source = post_source, data_desc = post_desc
-	, data_sub = subreddit) 
+	, data_sub = subreddit)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run()
